bd0be1a2d4a2147632ba2e452089762d1e28b319
poswalsameer <poswalsameer2003@gmail.com>
2024-02-17 20:31:42 +0530

    first commit of this project

---
+++ newsapp/src/app/components/BlogCard.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function BlogCard() {
+  return (
+    <div className='h-80 w-80 bg-red-700'>
+      
+    </div>
+  )
+}
+
+export default BlogCard
---
+++ newsapp/src/app/page.js
@@ -0,0 +1,16 @@
+import Image from "next/image";
+import BlogCard from "./components/BlogCard";
+
+
+export default function Home() {
+  return (
+    <>
+
+      <h1 className="text-4xl font-extrabold text-white text-center my-20">NEWS APP</h1>
+
+      <BlogCard />
+
+
+    </>
+  );
+}
