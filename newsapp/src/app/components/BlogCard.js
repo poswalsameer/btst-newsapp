7bacb1cb916013538ccb0abc142912b4ad85d797
poswalsameer <poswalsameer2003@gmail.com>
2024-02-18 15:44:45 +0530

    json is working now, mapped all the details on UI

--- newsapp/src/app/components/BlogCard.js
+++ newsapp/src/app/components/BlogCard.js
@@ -1,9 +1,26 @@
 import React from 'react'
 
-function BlogCard() {
+function BlogCard(props) {
   return (
-    <div className='h-80 w-80 bg-red-700'>
-      
+    <div className=' my-10 h-[30rem] w-80 flex flex-col'>
+
+        <div className='h-[17rem] w-full flex justify-center items-center bg-gray-500 rounded-t-2xl'><img src={props.image} alt="" className='h-full w-full rounded-t-2xl' /></div>
+
+        <div className='h-[13rem] w-full flex flex-col justify-center items-center bg-blue-500 rounded-b-2xl' >
+
+            <h1 className='text-lg flex justify-center items-center  h-40 w-full'>{props.title.slice(0, 30)}...</h1>
+
+            <p className='p-4 h-64 w-full text-sm flex justify-center items-center' >{ props.description && props.description.slice(0, 100)}...</p>
+
+            <button className='h-24 w-24 bg-red-300 rounded-md text-sm ' >Read More</button>
+
+            <div className=' w-full flex flex-row justify-around items-center my-3'>
+              <h1 className='text-xs'>{props.author}</h1>
+              <h1 className='text-xs'>{props.publishDate}</h1>
+            </div>
+
+        </div>
+
     </div>
   )
 }
--- newsapp/src/app/page.js
+++ newsapp/src/app/page.js
@@ -1,15 +1,59 @@
+"use client";
+
 import Image from "next/image";
+import { useEffect, useState } from "react";
 import BlogCard from "./components/BlogCard";
 
 
 export default function Home() {
+
+  // const [jsonData, setJsonData] = useState([]);
+  let jsonData;
+
+  const [blogArr, setBlogArr] = useState([]);
+  
+  useEffect( () => {
+
+      const getBlogData = async () => {
+
+        let blogURL = 'https://newsapi.org/v2/top-headlines?country=in&category=technology&apiKey=746cb39f1bce49cba5078e76def5e243';
+  
+        let blogData = await fetch(blogURL);
+    
+        if(!blogData.ok){
+          console.log("error while fetching");
+        }
+    
+        jsonData = await blogData.json();
+        // setJsonData(jsonData.articles);
+        console.log(jsonData);
+
+        setBlogArr(jsonData.articles);
+        console.log(blogArr);
+
+      }
+
+      getBlogData();
+
+  }, []);
+
+  
+  
+
   return (
     <>
 
       <h1 className="text-4xl font-extrabold text-white text-center my-20">NEWS APP</h1>
 
-      <BlogCard />
+      <div className="grid grid-cols-3 place-items-center items-center ">
 
+        {blogArr.map( (blog) => (
+
+          <BlogCard key={blog.url} image={blog.urlToImage} title={blog.title} description={blog.description} author={blog.author} publishDate={blog.publishedAt} />
+
+        ) )}
+
+      </div>
 
     </>
   );
